1982b0e3df1c068525e91f43bbffcf6b92a09300
Matheus Nunes <103690968+matheusnsilvab@users.noreply.github.com>
2022-05-09 01:44:07 -0300

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,19 @@
+function calculate() {
+  var altura = document.getElementById("altura").value / 100;
+  var peso = document.getElementById("peso").value;
+
+  var imc = (peso / (altura * altura)).toFixed(2);
+  var text=""
+  if (imc < 18.5) {
+    text=("Você está magro!")
+  } else if (imc < 24.9) {
+    text=("Você está normal!")
+  } else if (imc < 29.9) {
+    text=("Você está com sobrepeso!")
+  } else if (imc < 39.9) {
+    text=("Você está com obesidade!")
+  } else if (imc > 39.9) {
+    text=("Você está com obesidade mórbida!")
+  }
+  document.getElementById("text_area").innerText=text
+}
